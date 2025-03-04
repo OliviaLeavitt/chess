da22eb7e8f00d7ebf4383dcd89ce15077a715c55
“Olivia <ttivael@byu.edu>
2025-03-03 18:21:37 -0700

    added more tests

--- server/src/test/java/service/JoinServiceTest.java
+++ server/src/test/java/service/JoinServiceTest.java
@@ -10,14 +10,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.results.CreateResult;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class JoinServiceTest {
     private final MemoryUserDAO userDAO = new MemoryUserDAO();
     private final MemoryAuthDAO authDAO = new MemoryAuthDAO();
     private final MemoryGameDAO gameDAO = new MemoryGameDAO();
     private final CreateService createService = new CreateService(authDAO, gameDAO);
+    private final JoinService joinService = new JoinService(authDAO, gameDAO);
     private final ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
 
     @BeforeEach
@@ -30,7 +30,7 @@ public class JoinServiceTest {
         String username = "testUser";
         authDAO.createAuth(new Auth(authToken, username));
 
-        Game gameData = new Game(1, "whiteUsername", "blackUsername", "TestGame", new chess.ChessGame());
+        Game gameData = new Game(1, null, "blackUsername", "TestGame", new chess.ChessGame());
         CreateResult gameResult = createService.createGame(authToken, gameData);
         assertNotNull(gameResult);
 
@@ -38,6 +38,67 @@ public class JoinServiceTest {
         assertNotNull(createdGame);
         assertEquals("TestGame", createdGame.gameName());
 
+        joinService.joinGame(authToken, "WHITE", createdGame.gameID());
+
+        Game updatedGame = gameDAO.getGame(gameResult.gameID());
+        assertEquals(username, updatedGame.whiteUsername());
 
     }
+    @Test
+    void joinUnauthorizedGame() {
+        String authToken = "testAuthToken";
+        try {
+            joinService.joinGame(authToken, "WHITE", 1);
+            fail("Exception should have been thrown");
+        } catch (ResponseException exception) {
+            assertEquals(401, exception.statusCode());
+        }
+    }
+    @Test
+    void joinNoGameDataGame() {
+        String authToken = "testAuthToken";
+        String username = "testUser";
+        authDAO.createAuth(new Auth(authToken, username));
+        try {
+            joinService.joinGame(authToken, "WHITE", 1);
+            fail("Exception should have been thrown");
+        } catch (ResponseException exception) {
+            assertEquals(400, exception.statusCode());
+        }
+    }
+    @Test
+    void joinNoPlayerColorGame() throws ResponseException {
+        String authToken = "testAuthToken";
+        String username = "testUser";
+        authDAO.createAuth(new Auth(authToken, username));
+
+        Game gameData = new Game(1, null, "blackUsername", "TestGame", new chess.ChessGame());
+        CreateResult gameResult = createService.createGame(authToken, gameData);
+        assertNotNull(gameResult);
+
+        try {
+            joinService.joinGame(authToken, null, gameResult.gameID());
+            fail("Exception should have been thrown");
+        } catch (ResponseException exception) {
+            assertEquals(400, exception.statusCode());
+        }
+    }
+    @Test
+    void joinAlreadyTakenGame() throws ResponseException {
+        String authToken = "testAuthToken";
+        String username = "testUser";
+        authDAO.createAuth(new Auth(authToken, username));
+
+        Game gameData = new Game(1, "whiteUsername", "blackUsername", "TestGame", new chess.ChessGame());
+        CreateResult gameResult = createService.createGame(authToken, gameData);
+        assertNotNull(gameResult);
+
+        try {
+            joinService.joinGame(authToken, "WHITE", gameResult.gameID());
+            fail("Exception should have been thrown");
+        } catch (ResponseException exception) {
+            assertEquals(403, exception.statusCode());
+        }
+    }
+
 }
--- server/src/test/java/service/ListServiceTest.java
+++ server/src/test/java/service/ListServiceTest.java
@@ -1,4 +1,73 @@
 package service;
 
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import exception.ResponseException;
+import model.Auth;
+import model.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.results.ListResult;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ListServiceTest {
+    private final MemoryUserDAO userDAO = new MemoryUserDAO();
+    private final MemoryAuthDAO authDAO = new MemoryAuthDAO();
+    private final MemoryGameDAO gameDAO = new MemoryGameDAO();
+    private final ListService listService = new ListService(authDAO, gameDAO);
+    private final ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
+    private final CreateService createService = new CreateService(authDAO, gameDAO);
+
+    @BeforeEach
+    void startFresh() throws ResponseException {
+        clearService.clear();
+    }
+
+    @Test
+    void listSuccessGames() throws ResponseException {
+        String authToken = "testAuthToken";
+        String username = "testUser";
+        authDAO.createAuth(new Auth(authToken, username));
+
+        Game gameData = new Game(1, "whiteUsername", "blackUsername", "TestGame", new chess.ChessGame());
+        Game gameData2 = new Game(2, "whiteUsername2", "blackUsername2", "TestGame2", new chess.ChessGame());
+        Game gameData3 = new Game(3, "whiteUsername3", "blackUsername3", "TestGame3", new chess.ChessGame());
+        createService.createGame(authToken, gameData);
+        createService.createGame(authToken, gameData2);
+        createService.createGame(authToken, gameData3);
+
+        ListResult listResult = listService.listGames(authToken);
+        assertNotNull(listResult);
+
+        Collection<Game> games = listResult.games();
+        assertNotNull(games);
+        assertEquals(3, games.size());
+    }
+    @Test
+    void listUnauthorizedGames() throws ResponseException {
+        try {
+            listService.listGames("invalidAuthToken");
+            fail("Exception should have been thrown");
+        } catch (ResponseException exception) {
+            assertEquals(401, exception.statusCode());
+        }
+    }
+    @Test
+    void listEmptyGames() throws ResponseException {
+        String authToken = "testAuthToken";
+        String username = "testUser";
+        authDAO.createAuth(new Auth(authToken, username));
+
+        ListResult listResult = listService.listGames(authToken);
+        assertNotNull(listResult);
+
+        Collection<Game> games = listResult.games();
+        assertNotNull(games);
+        assertEquals(0, games.size());
+    }
+
 }
