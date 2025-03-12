36b88b44176c4fadb00231d6977204e4e08e07ef
Olivia Leavitt <165630133+OliviaLeavitt@users.noreply.github.com>
2025-03-11 18:46:52 -0600

    added some auth functions but they don't work fully

--- server/src/main/java/dataaccess/mysql/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/mysql/MySQLAuthDAO.java
@@ -5,7 +5,10 @@ import dataaccess.DataAccessException;
 import dataaccess.DatabaseManager;
 import exception.ResponseException;
 import model.Auth;
+import model.User;
+import org.mindrot.jbcrypt.BCrypt;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
@@ -14,18 +17,39 @@ import static java.sql.Types.NULL;
 public class MySQLAuthDAO implements AuthDAO {
     @Override
     public void clear() throws ResponseException {
-        var statement = "TRUNCATE games";
+        var statement = "TRUNCATE auth";
         executeUpdate(statement);
     }
 
     @Override
     public Auth createAuth(Auth auth) throws ResponseException {
-        return null;
+        var statement = "INSERT INTO users (authToken, use) VALUES (?, ?, ?)";
+        executeUpdate(statement, auth.authToken(), auth.username());
+        return new Auth(auth.authToken(), auth.username());
     }
 
     @Override
     public Auth getAuth(String authToken) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM auth WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
+    }
+
+    private Auth readAuth(ResultSet rs) throws SQLException {
+        String authToken = rs.getString("authToken");
+        String username = rs.getString("username");
+        return new Auth(authToken, username);
     }
 
     @Override
