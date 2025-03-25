9d6fd55393e61caa130a2816102c5401a251a15b
Olivia Leavitt <165630133+OliviaLeavitt@users.noreply.github.com>
2025-03-24 21:33:34 -0600

    fixed folder structure

--- client/src/main/java/Repl.java
+++ client/src/main/java/Repl.java
@@ -1,42 +1,4 @@
-package client;
-
-import client.websocket.NotificationHandler;
-import webSocketMessages.Notification;
-
-import java.util.Scanner;
-
-import static client.EscapeSequences.*;
+package PACKAGE_NAME;
 
 public class Repl {
-    private final ChessClient client;
-
-    public Repl(String serverUrl) {
-        client = new ChessClient(serverUrl, this);
-    }
-
-    public void run() {
-        System.out.println("Welcome to chess. type HELP to get started.");
-        System.out.print(client.help());
-
-        Scanner scanner = new Scanner(System.in);
-        var result = "";
-        while (!result.equals("quit")) {
-            printPrompt();
-            String line = scanner.nextLine();
-
-            try {
-                result = client.eval(line);
-                System.out.print(BLUE + result);
-            } catch (Throwable e) {
-                var msg = e.toString();
-                System.out.print(msg);
-            }
-        }
-        System.out.println();
-    }
-
-    private void printPrompt() {
-        System.out.print("\n" + RESET + ">>> " + GREEN);
-    }
-
 }
