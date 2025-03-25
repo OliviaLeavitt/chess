a8a74b1eb52e3b257c517bfab7533f195de9e4ad
Olivia Leavitt <165630133+OliviaLeavitt@users.noreply.github.com>
2025-03-24 19:39:33 -0600

    working version with added functions for phase5

---
+++ client/src/main/java/ChessClient.java
@@ -0,0 +1,160 @@
+//package client;
+//
+//import java.util.Arrays;
+//
+//import com.google.gson.Gson;
+//import model.Pet;
+//import model.PetType;
+//import exception.ResponseException;
+//import server.ServerFacade;
+//import ui.State;
+//
+//public class ChessClient {
+//    private final ServerFacade server;
+//    private String authToken = null;
+//    private final String serverUrl;
+//    private State state = State.PRELOGIN;
+//
+//    public ChessClient(String serverUrl) {
+//        server = new ServerFacade(serverUrl);
+//        this.serverUrl = serverUrl;
+//    }
+//
+//    public String eval(String input) {
+//        try {
+//            var tokens = input.toLowerCase().split(" ");
+//            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+//            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+//            if (state == State.PRELOGIN) {
+//                return handlePreloginCommands(cmd, params);
+//            } else if (state == State.POSTLOGIN) {
+//                return handlePostloginCommands(cmd, params);
+//            } else {
+//                throw new IllegalStateException("Invalid client state: " + state);
+//            }
+//        } catch (ResponseException ex) {
+//            return ex.getMessage();
+//        }
+//    }
+//
+//    private String handlePreloginCommands(String cmd, String... params) throws ResponseException {
+//        return switch (cmd) {
+//            case "register" -> register(params);
+//            case "login" -> login(params);
+//            case "quit" -> "quit";
+//            default -> help();
+//        };
+//    }
+//
+//    private String handlePostloginCommands(String cmd, String... params) throws ResponseException {
+//        return switch (cmd) {
+//            case "logout" -> logout();
+//            case "creategame" -> createGame(params);
+//            case "listgames" -> listGames();
+//            case "playgame" -> playGame(params);
+//            case "observegame" -> observeGame(params);
+//            case "help" -> help();
+//            case "quit" -> "quit";
+//            default -> help();
+//        };
+//    }
+//
+//    public String login(String... params) throws ResponseException {
+//        if (params.length == 2) {
+//            LoginRequest loginRequest = new LoginRequest(params[0], params[1]);
+//            LoginResult loginResult = server.login(loginRequest);
+//            state = State.POSTLOGIN;
+//            return String.format("Logged in as %s.", params[0]);
+//        }
+//        throw new ResponseException(400, "Expected: <yourname>");
+//    }
+//
+//    public String rescuePet(String... params) throws ResponseException {
+//        assertSignedIn();
+//        if (params.length >= 2) {
+//            var name = params[0];
+//            var type = PetType.valueOf(params[1].toUpperCase());
+//            var pet = new Pet(0, name, type);
+//            pet = server.addPet(pet);
+//            return String.format("You rescued %s. Assigned ID: %d", pet.name(), pet.id());
+//        }
+//        throw new ResponseException(400, "Expected: <name> <CAT|DOG|FROG>");
+//    }
+//
+//    public String listPets() throws ResponseException {
+//        assertSignedIn();
+//        var pets = server.listPets();
+//        var result = new StringBuilder();
+//        var gson = new Gson();
+//        for (var pet : pets) {
+//            result.append(gson.toJson(pet)).append('\n');
+//        }
+//        return result.toString();
+//    }
+//
+//    public String adoptPet(String... params) throws ResponseException {
+//        assertSignedIn();
+//        if (params.length == 1) {
+//            try {
+//                var id = Integer.parseInt(params[0]);
+//                var pet = getPet(id);
+//                if (pet != null) {
+//                    server.deletePet(id);
+//                    return String.format("%s says %s", pet.name(), pet.sound());
+//                }
+//            } catch (NumberFormatException ignored) {
+//            }
+//        }
+//        throw new ResponseException(400, "Expected: <pet id>");
+//    }
+//
+//    public String adoptAllPets() throws ResponseException {
+//        assertSignedIn();
+//        var buffer = new StringBuilder();
+//        for (var pet : server.listPets()) {
+//            buffer.append(String.format("%s says %s%n", pet.name(), pet.sound()));
+//        }
+//
+//        server.deleteAllPets();
+//        return buffer.toString();
+//    }
+//
+//    public String signOut() throws ResponseException {
+//        assertSignedIn();
+//        ws.leavePetShop(visitorName);
+//        ws = null;
+//        state = State.SIGNEDOUT;
+//        return String.format("%s left the shop", visitorName);
+//    }
+//
+//    private Pet getPet(int id) throws ResponseException {
+//        for (var pet : server.listPets()) {
+//            if (pet.id() == id) {
+//                return pet;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public String help() {
+//        if (state == State.SIGNEDOUT) {
+//            return """
+//                    - signIn <yourname>
+//                    - quit
+//                    """;
+//        }
+//        return """
+//                - list
+//                - adopt <pet id>
+//                - rescue <name> <CAT|DOG|FROG|FISH>
+//                - adoptAll
+//                - signOut
+//                - quit
+//                """;
+//    }
+//
+//    private void assertSignedIn() throws ResponseException {
+//        if (state == State.SIGNEDOUT) {
+//            throw new ResponseException(400, "You must sign in");
+//        }
+//    }
